da9359ef152cdb5026115e439a59d95dd0b9304a
Somge0410 <aaronschaefers@web.de>
2026-06-13 09:51:20 +0200

    before spsa

--- include/SPSA_parameters.h
+++ include/SPSA_parameters.h
@@ -7,13 +7,13 @@ inline int DELTA_MARGIN = 200;
 inline int MAX_QUIET_PLY = 7;
 
 // --- NEU: Late Move Reduction (LMR) ---
-inline int LMR_MIN_DEPTH = 0;
-inline int LMR_MIN_MOVES_SEARCHED = 1;
-inline int LMR_REDUCTION_AMOUNT = 64;
+inline int LMR_MIN_DEPTH = 3;
+inline int LMR_MIN_MOVES_SEARCHED = 3;
+inline int LMR_REDUCTION_AMOUNT = 2;
 
 // --- NEU: Null Move Pruning (NMP) ---
-inline int NMP_MIN_DEPTH = 0;
-inline int NMP_REDUCTION = 1;
+inline int NMP_MIN_DEPTH = 3;
+inline int NMP_REDUCTION = 3;
 
 // --- NEU: Move Ordering ---
 inline int TT_STAGE = 7;
@@ -37,19 +37,19 @@ inline double ASPIRATION_WINDOW_MULTIPLIER = 2;
 inline double OPT_TIME_ALLOCATION_DIVISOR = 40;     // time_left / divisor
 inline double OPT_TIME_ALLOCATION_DIVISOR_MG = 40;  // time_left / divisor
 inline double OPT_TIME_ALLOCATION_DIVISOR_EG = 40;  // time_left / divisor
-inline double MAX_TIME_ALLOCATION_DIVISOR = 40;
-inline double MAX_TIME_ALLOCATION_DIVISOR_MG = 40;
-inline double MAX_TIME_ALLOCATION_DIVISOR_EG = 40;
+inline double MAX_TIME_ALLOCATION_DIVISOR = 30;
+inline double MAX_TIME_ALLOCATION_DIVISOR_MG = 30;
+inline double MAX_TIME_ALLOCATION_DIVISOR_EG = 30;
 constexpr int MAX_RECENT_BEST_COUNT = 5;
 inline double TIME_CHANGES_COUNT_BIG = 0.1;
 inline double TIME_CHANGES_COUNT_MEDIUM = 0.05 ;
 inline double TIME_CHANGES_COUNT_SMALL = 0.01;
-inline double INCREMENT_DIVISOR = 1;
+inline double INCREMENT_DIVISOR = 1.1;
 inline int DELTA_BEST_SCORE = 300;
-inline double VOLATILITY_DIV = 1;
-inline double EXTRA_BEST_BASE = 0;
-inline double EXTRA_BEST_FLIP = 0;
-inline double EXTRA_BEST_WEIGHT = 0;
+inline double VOLATILITY_DIV = 250;
+inline double EXTRA_BEST_BASE = 0.05;
+inline double EXTRA_BEST_FLIP = 0.1;
+inline double EXTRA_BEST_WEIGHT = 1;
 inline double NO_TIME_TRIGGER_DIV = 2;
 inline double NO_TIME_ALLOC_DIV = 40;
 inline double MAX_NO_TIME_ALLOC_DIV = 40;
@@ -57,7 +57,7 @@ inline double TIME_MARGIN = 0.5;
 inline double LOG_BASE = 0.2;
 inline double LOG_DIV = 3.35;
 inline double Q_LOG_BASE = 1.35;
-inline double Q_LOG_DIV = 2.75;
+inline double Q_LOG_DIV = 3.35;
 
 // --- NEU: Root Move Perturbation (Multi-Threading) ---
 inline int ROOT_PERTURBATION_MIN_HELPERS = 2;
