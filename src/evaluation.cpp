54a6b6aa5a5b9cf990f8072b01d0c6e3c01ce755
Somge0410 <aaronschaefers@web.de>
2026-06-27 07:34:10 +0200

    jo

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -24,7 +24,6 @@ int evaluate(const Board& board, Trace* trace, uint8_t terms_mask) {
 	eval_material<isTracing>(score, board, trace);
 	eval_positional<isTracing>(score, board, trace);
 	eval_pawns<isTracing>(score, ctx, trace);
-	return tapered(score, board.get_game_phase());
 	if (terms_mask != EvalAll) return tapered(score, board.get_game_phase());
 	eval_king_safety<isTracing>(score, ctx, trace);
 	eval_mobility<isTracing>(score, ctx, trace);
