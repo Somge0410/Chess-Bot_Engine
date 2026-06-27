490b13f65db1b9fd3e2ea56cf9ea3209f337236b
Somge0410 <aaronschaefers@web.de>
2026-06-27 09:23:42 +0200

    king safety

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -26,6 +26,8 @@ int evaluate(const Board& board, Trace* trace, uint8_t terms_mask) {
 	eval_pawns<isTracing>(score, ctx, trace);
 	if (terms_mask != EvalAll) return tapered(score, board.get_game_phase());
 	eval_king_safety<isTracing>(score, ctx, trace);
+
+	return tapered(score, board.get_game_phase());
 	eval_mobility<isTracing>(score, ctx, trace);
 	eval_rook_activity<isTracing>(score, ctx, trace);
 	eval_minor_pieces<isTracing>(score, ctx, trace);
