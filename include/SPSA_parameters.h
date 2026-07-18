6120dd1a339abe4b01d473bdb9ee10916224faa6
Somge0410 <aaronschaefers@web.de>
2026-07-18 10:25:15 +0200

    test

--- include/SPSA_parameters.h
+++ include/SPSA_parameters.h
@@ -1,9 +1,9 @@
 #pragma once
 
 // For Pruning and search
-inline int FUTILITY_MARGIN_D1 = 204;
-inline int FUTILITY_MARGIN_D2 = 434;
-inline int DELTA_MARGIN = 180;
+inline int FUTILITY_MARGIN_D1 = 244;
+inline int FUTILITY_MARGIN_D2 = 460;
+inline int DELTA_MARGIN = 179;
 inline int MAX_QUIET_PLY = 7;
 
 // --- NEU: Late Move Reduction (LMR) ---
@@ -27,36 +27,36 @@ inline int LOSING_CAPTURE_STAGE = 1;
 inline int CAPTURE_SCORE_TIEBREAK_DIVISOR = 16;
 
 // --- NEU: History Heuristic ---
-inline int HISTORY_BONUS_MULTIPLIER = 6;  // bonus = depth * depth * multiplier
+inline int HISTORY_BONUS_MULTIPLIER = 8;  // bonus = depth * depth * multiplier
 
 // --- NEU: Aspiration Window ---
-inline int ASPIRATION_WINDOW_INITIAL = 52;
-inline double ASPIRATION_WINDOW_MULTIPLIER = 2.7;
+inline int ASPIRATION_WINDOW_INITIAL = 45;
+inline double ASPIRATION_WINDOW_MULTIPLIER = 2.662;
 
 // --- NEU: Time Management ---
-inline double OPT_TIME_ALLOCATION_DIVISOR = 42.374;     // time_left / divisor
-inline double OPT_TIME_ALLOCATION_DIVISOR_MG = 34.3507;  // time_left / divisor
-inline double OPT_TIME_ALLOCATION_DIVISOR_EG = 37.0596;  // time_left / divisor
-inline double MAX_TIME_ALLOCATION_DIVISOR = 29.2642;
-inline double MAX_TIME_ALLOCATION_DIVISOR_MG = 30.4544;
-inline double MAX_TIME_ALLOCATION_DIVISOR_EG = 28.8201;
+inline double OPT_TIME_ALLOCATION_DIVISOR = 33.69;     // time_left / divisor
+inline double OPT_TIME_ALLOCATION_DIVISOR_MG = 12.9;  // time_left / divisor
+inline double OPT_TIME_ALLOCATION_DIVISOR_EG = 29.6;  // time_left / divisor
+inline double MAX_TIME_ALLOCATION_DIVISOR = 28.7;
+inline double MAX_TIME_ALLOCATION_DIVISOR_MG = 32.27;
+inline double MAX_TIME_ALLOCATION_DIVISOR_EG = 28.5;
 constexpr int MAX_RECENT_BEST_COUNT = 5;
 inline double TIME_CHANGES_COUNT_BIG = 0.1;
-inline double TIME_CHANGES_COUNT_MEDIUM = 0.0497 ;
-inline double TIME_CHANGES_COUNT_SMALL = 0.006;
-inline double INCREMENT_DIVISOR = 1.13;
-inline int DELTA_BEST_SCORE = 284;
-inline double VOLATILITY_DIV = 267;
-inline double EXTRA_BEST_BASE = 0.0541;
-inline double EXTRA_BEST_FLIP = 0.0823;
-inline double EXTRA_BEST_WEIGHT = 0.998;
-inline double NO_TIME_TRIGGER_DIV = 1.9373;
-inline double NO_TIME_ALLOC_DIV = 40;
-inline double MAX_NO_TIME_ALLOC_DIV = 40;
-inline double TIME_MARGIN = 0.3942;
-inline double LOG_BASE = 0.206;
-inline double LOG_DIV = 3.3591;
-inline double Q_LOG_BASE = 1.36;
+inline double TIME_CHANGES_COUNT_MEDIUM = 0.0404 ;
+inline double TIME_CHANGES_COUNT_SMALL = 0.0017;
+inline double INCREMENT_DIVISOR = 1.1238;
+inline int DELTA_BEST_SCORE = 270;
+inline double VOLATILITY_DIV = 202.38;
+inline double EXTRA_BEST_BASE = 0.0731;
+inline double EXTRA_BEST_FLIP = 0.0015;
+inline double EXTRA_BEST_WEIGHT = 1.0081;
+inline double NO_TIME_TRIGGER_DIV = 2.01;
+inline double NO_TIME_ALLOC_DIV = 42.9;
+inline double MAX_NO_TIME_ALLOC_DIV = 40.34;
+inline double TIME_MARGIN = 0.3916;
+inline double LOG_BASE = 0.209;
+inline double LOG_DIV = 3.36;
+inline double Q_LOG_BASE = 1.37;
 inline double Q_LOG_DIV = 3.356;
 
 // --- NEU: Root Move Perturbation (Multi-Threading) ---
@@ -64,8 +64,8 @@ inline int ROOT_PERTURBATION_MIN_HELPERS = 2;
 inline int ROOT_PERTURBATION_MIN_BAND_SIZE = 6;
 inline int ROOT_PERTURBATION_MAX_BAND_SIZE = 16;
 
-inline int REVERSE_FUTILITY_MAX_DEPTH = 6;
-inline int REVERSE_FUTILITY_MARGIN = 137;
+inline int REVERSE_FUTILITY_MAX_DEPTH = 7;
+inline int REVERSE_FUTILITY_MARGIN = 138;
 
 inline int PAWN_PUSH_SCORE1 = 40;
 inline int PAWN_PUSH_SCORE2 = 20;
