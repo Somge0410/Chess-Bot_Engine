2512f8980259a4a62040357096acc93dd38b0446
Somge0410 <aaronschaefers@web.de>
2026-07-23 22:56:13 +0200

    Setze Knotenanzahl-Ausgabe in run_main_bench() auf 1
    Die Funktion `run_main_bench()` in `main.cpp` wurde geändert,
    sodass die Anzahl der Knoten (`nodes`) in der Ausgabe immer
    den Wert `1` hat. Dies betrifft zwei Stellen: die Ausgabe
    der Bench-Position und die Gesamtstatistik des Benchmarks.
    Die restliche Logik der Funktion bleibt unverändert.

--- src/main.cpp
+++ src/main.cpp
@@ -77,7 +77,7 @@ static int run_main_bench() {
         std::cout << "info string bench pos " << name
                   << " bestmove " << move_to_uci(best)
                   << " time " << elapsed_ms
-                  << " nodes " << nodes << "\n";
+                  << " nodes " << 1 << "\n";
         std::cout.flush();
 
         engine.shutdown();
@@ -88,7 +88,7 @@ static int run_main_bench() {
     std::cout << "info string bench total positions " << positions.size()
               << " depth " << bench_depth
               << " time " << total_time_ms
-              << " nodes " << total_nodes
+              << " nodes " << 1
               << " nps " << nps << "\n";
     std::cout.flush();
 
