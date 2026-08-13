90d1dd527510797f9514bd314267401095dfa9f3
Somge0410 <aaronschaefers@web.de>
2026-08-13 18:39:37 +0200

    changed q-cap to 25

--- include/engine.h
+++ include/engine.h
@@ -133,7 +133,7 @@ struct SearchResult {
 class Engine;
 struct ThreadLocalData {
     static constexpr uint32_t TIME_CHECK_INTERVAL = 1024;
-    static constexpr int QSEARCH_PLY_CAPACITY = 33;
+    static constexpr int QSEARCH_PLY_CAPACITY = 25;
 
     void clear_counters() {
         nodes = 0;
